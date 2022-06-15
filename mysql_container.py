0f5abd832c70e74ce3f42b6e3c943f70ec1413f8
Joseph <jvalencianom08@estudiantec.cr>
2022-06-15 13:13:55 -0600

    Changing metadata to sql

--- mysql_container.py
+++ mysql_container.py
@@ -2,21 +2,29 @@ from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine
 from testcontainers.mysql import MySqlContainer
 
 
-meta = MetaData()
+# meta = MetaData()
 
-students = Table(
-   'students', meta, 
-   Column('id', Integer, primary_key = True), 
-   Column('name', String(20)), 
-   Column('lastname', String(30)),
-)
+# students = Table(
+#    'students', meta, 
+#    Column('id', Integer, primary_key = True), 
+#    Column('name', String(20)), 
+#    Column('lastname', String(30)),
+# )
 
 with MySqlContainer('mysql:5.7.17') as mysql:
     engine = create_engine(mysql.get_connection_url())
     version, = engine.execute("select version()").fetchone()
     print(version)  # 5.7.17
-    meta.create_all(engine)
+    engine.execute("""
+                    CREATE TABLE students (
+                    id int,
+                    name varchar(30),
+                    lastname varchar(40)
+                                        )
+                """)
+    # meta.create_all(engine)
     engine.execute("insert into students values (1, 'Joseph', 'Valenciano')")
+    engine.execute("insert into students values (2, 'Alejandro', 'Rojas')")
     result = engine.execute("select name from students")
     for row in result:
         print("name:", row['name'])
