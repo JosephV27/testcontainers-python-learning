3b30b85a2cd6da6d918a60887f2e468bd30977d6
Joseph <jvalencianom08@estudiantec.cr>
2022-06-15 12:28:26 -0600

    Adding sql querys to test the database

--- mysql_container.py
+++ mysql_container.py
@@ -1,11 +1,24 @@
-import sqlalchemy
+from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine
 from testcontainers.mysql import MySqlContainer
 
-with MySqlContainer('mysql:5.7.17') as mysql:
-    engine = sqlalchemy.create_engine(mysql.get_connection_url())
-    version, = engine.execute("select version()").fetchone()
-    date, = engine.execute("select now();").fetchone()
-    print(version)  # 5.7.17
-    print(date)
 
+meta = MetaData()
+
+students = Table(
+   'students', meta, 
+   Column('id', Integer, primary_key = True), 
+   Column('name', String(20)), 
+   Column('lastname', String(30)),
+)
+
+with MySqlContainer('mysql:5.7.17') as mysql:
+    engine = create_engine(mysql.get_connection_url())
+    version, = engine.execute("select version()").fetchone()
+    print(version)  # 5.7.17
+    meta.create_all(engine)
+    engine.execute("insert into students values (1, 'Joseph', 'Valenciano')")
+    result = engine.execute("select name from students")
+    for row in result:
+        print("name:", row['name'])
+    result.close()
 
