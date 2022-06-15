9957ea956bce216e775ee56665b63e584fae2863
Joseph <jvalencianom08@estudiantec.cr>
2022-06-15 13:15:42 -0600

    Deleting comments

--- mysql_container.py
+++ mysql_container.py
@@ -1,15 +1,5 @@
-from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine
+from sqlalchemy import create_engine
 from testcontainers.mysql import MySqlContainer
-
-
-# meta = MetaData()
-
-# students = Table(
-#    'students', meta, 
-#    Column('id', Integer, primary_key = True), 
-#    Column('name', String(20)), 
-#    Column('lastname', String(30)),
-# )
 
 with MySqlContainer('mysql:5.7.17') as mysql:
     engine = create_engine(mysql.get_connection_url())
@@ -22,7 +12,6 @@ with MySqlContainer('mysql:5.7.17') as mysql:
                     lastname varchar(40)
                                         )
                 """)
-    # meta.create_all(engine)
     engine.execute("insert into students values (1, 'Joseph', 'Valenciano')")
     engine.execute("insert into students values (2, 'Alejandro', 'Rojas')")
     result = engine.execute("select name from students")
